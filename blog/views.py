4fec31a30cf336ec8606acd8e7dd0dd88f2ac007
lizzzcai <lilcolinn@gmail.com>
2020-11-03 14:33:00 +0800

    update view of the home page to query post list

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
+    post_list = Post.objects.all().order_by('-created_time') # add - means order reversely
     return render(request, 'blog/index.html', context={
         'title': 'My Blog Page',
-        'welcome': 'Welcome to my blog!'
+        'welcome': 'Welcome to my blog!',
+        'post_list': post_list
     })
