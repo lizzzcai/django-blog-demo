b8fe91ca602cb7a91c21ec2dcc07fbbb5d4b9e91
lizzzcai <lilcolinn@gmail.com>
2020-11-03 14:25:29 +0800

    create urls for blog, and create view for blog home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': 'My Blog Page',
+        'welcome': 'Welcome to my blog!'
+    })
